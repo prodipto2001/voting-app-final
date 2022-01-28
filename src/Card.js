397e5f8821839fbb73d9e36b7419ccda2dcff1e1
Prodipto <prodipto2001@gmail.com>
2022-01-28 14:02:03 +0530

    Second Commit

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,48 @@
 import react from 'react';
-import './Card.css';
+import { Box, Image, Button} from '@chakra-ui/react' 
 
-function Card() {
+function Card( {userName, userPicture} ){
+  const details = {
+    imageUrl: userPicture,
+    Name: userName,
+  }
+
   return (
-    <div className="card">
-        <img src = 'https://images.app.goo.gl/5eCq6baTLnsfSskL6' />
-        <h1>John Doe</h1>
-        <button>VOTE</button>
-    </div>
-    );
+    <Box maxW='s' borderWidth='1px' borderRadius='20px' boxShadow='0px 4px 10px rgba(0, 0, 0, 0.25)' padding='20px' overflow='hidden' alignItems='center' display='flex' flexDirection='column'>
+        
+        <Image
+        boxSize='300px'
+        borderRadius='full'
+        objectFit='cover'
+        src={details.imageUrl}
+        alt={details.Name}
+        />
+
+        <Box
+          mt='3'
+          fontWeight='bold'
+          as='h2'
+          lineHeight='tight'
+          isTruncated
+          fontSize='xl'
+          
+        >
+          {details.Name}
+        </Box>
+
+        <Box display='flex' mt='2' justifyContent='center'>
+            <Button size='md'
+              height='75px'
+              width='323px'
+              borderRadius = '50px'
+              backgroundColor ='#FF8A00' 
+            >
+              VOTE
+            </Button>
+        </Box>
+        
+    </Box>
+  )
 }
 
 export default Card;
